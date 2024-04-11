139443c0391727333c3784a3ee9ddd30373d827f
soumika-m <soumika1999@gmail.com>
2024-04-12 00:03:15 +0530

    Added print divisors, digit sum, prime sum till N - 11/04/24

--- MathProblem/BasicProblem.java
+++ MathProblem/BasicProblem.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Collections;
 public class BasicProblem {
 
 /* (1)
@@ -25,6 +27,7 @@ public class BasicProblem {
 
 /* (2)
 * Return "Yes" if it is a armstrong number else return "No".
+* https://practice.geeksforgeeks.org/problems/armstrong-numbers2727/1
 * T(c) -> O(1) // as n will be always 3 digit number (constant)
 * if n is not constant (variable in nature), T(c) -> O(logn)
 */    
@@ -51,6 +54,7 @@ public class BasicProblem {
 /* (3)
  * Given a mathematical equation that contains only numbers and +, -, *, /. 
  * Print the equation in reverse, such that the equation is reversed, but the numbers remain the same.
+ * https://practice.geeksforgeeks.org/problems/reversing-the-equation2205/1
  * T(c) -> O(N) / O(|S|)
  */
 
@@ -81,6 +85,135 @@ public class BasicProblem {
         return res.toString();
     }
 
+/* (4)
+ * Given an integer N, print all the divisors of N in the ascending order.
+ * https://practice.geeksforgeeks.org/problems/all-divisors-of-a-number/1
+ * T(c) -> sqrt(N), space -> sqrt(N)
+ */
+    public static void printDivisors(int n) {
+        // code here
+        ArrayList<Integer> divisors = new ArrayList<>();
+        // run loop till sqrt(n)
+        for(int i=1;i*i<=n;i++)
+        {
+            // if i is divisors of n, add that to list
+            if(n%i == 0)
+            {
+                divisors.add(i);
+                // if another number(multiple) is not same as i, add to list
+                if(i != n/i)
+                {
+                    divisors.add(n/i);
+                }
+            }
+        }
+        
+        // sort the list
+        Collections.sort(divisors); 
+    
+        // print the divisors
+        for(int divisor:divisors)
+        {
+            System.out.print(divisor + " ");
+        }
+    }
+
+/* (5)
+ * Given a number n.Find if the digit sum(or sum of digits) of n is a Palindrome number or not.
+ * https://www.geeksforgeeks.org/problems/sum-of-digit-is-pallindrome-or-not2751/1
+ * T(c) -> O(log(n)) or O(d) where d is number of digits, space -> O(1)
+ */
+
+    static int isDigitSumPalindrome(int n) {
+        // code here
+        int temp = n;
+        int sum = 0;
+        // finding digit sum
+        while(temp > 0)
+        {
+            int digit = temp%10;
+            sum += digit;
+            temp = temp/10;
+        }
+        
+        if(isPalindrome(sum))
+        {
+            return 1;
+        }
+        else{
+            return 0;
+        }
+    }
+    
+    static boolean isPalindrome(int n)
+    {
+        int temp = n;
+        int rev = 0;
+        while(temp > 0)
+        {
+            int digit = temp%10;
+            rev = (rev*10)+digit;
+            temp = temp/10;
+        }
+        
+        // if reversed, the number stays same as before
+        if(n == rev)
+        {
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+/* (6)
+ * Given an integer num, repeatedly add all its digits until the result has only one digit, and return it.
+ * https://leetcode.com/problems/add-digits/
+ * T(c) -> O(log(n)) or O(d) where d is number of digits, space -> O(1)
+ */ 
+
+    static int addDigits(int num) {
+        while(num>=10)
+        {
+            num = digitSum(num); 
+        }
+        return num;
+    }
+    
+    static int digitSum(int num)
+    {
+        int temp = num;
+        int sum = 0;
+        while(temp > 0)
+        {
+            int digit = temp%10;
+            sum += digit;
+            temp = temp/10;
+        }
+        return sum;
+    }
+
+    /* 
+    * T(c) -> O(1) 
+    */
+    public int addDigitsEfficient(int num) {
+    //  if number is 0, return 0
+        if(num == 0)
+        {
+            return 0;
+        }
+
+    //  using digital root formula - dr(n)=n%9 or dr(n)=1+(n-1)%9
+    //  if number is divisible by 9, we need to return 9 instead of 0
+        if(num % 9 == 0)
+        {
+            return 9;
+        }
+        else{
+            return num % 9;
+        }
+    }
+
     public static void main(String args[])
     {
         int N = 12;
@@ -89,5 +222,13 @@ public class BasicProblem {
         System.out.println("Armstrong Number? "+ N +" = "+  armstrongNumber(N));
         String eq1 = "20-3+5*2";
         System.out.println("Reverse Equation? "+ eq1 +" = "+  reverseEqn(eq1));
+        N = 20;
+        System.out.print("Printing Divisors of "+ N +" = ");
+        printDivisors(N);
+        System.out.println();
+        N = 56;
+        System.out.println("Sum of digits is Palindrome Number? "+ N +" = "+  isDigitSumPalindrome(N));
+        N = 2147483647;  // 2^31-1
+        System.out.println("After adding digits of "+ N +" repeatedly this single digit generated = "+  addDigits(N));
     }
 }
--- MathProblem/PrimeNumber.java
+++ MathProblem/PrimeNumber.java
@@ -1,9 +1,13 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 class PrimeNumber
 {
-    /* Return 0 for not prime, 1 for prime
-     * https://www.geeksforgeeks.org/problems/prime-number2314/1
-     * T(c) -> o(N)
-     */
+/* (1)
+    * Return 0 for not prime, 1 for prime
+    * https://www.geeksforgeeks.org/problems/prime-number2314/1
+    * T(c) -> o(N)
+*/
     static int isPrime(int N)
     {
         if(N == 0 || N == 1)
@@ -42,9 +46,90 @@ class PrimeNumber
         return 1;
     }
 
+/* (2)
+ * Sum of all prime numbers between 1 and N.
+ * https://www.geeksforgeeks.org/problems/sum-of-all-prime-numbers-between-1-and-n4404/1
+ * T(c) -> O(n sqrt(n)) , space -> O(n)
+ */
+
+    public static long prime_Sum(int n)
+    {
+        // code here
+        ArrayList<Integer> primeNumbers = new ArrayList<>();
+        for(int i=2;i<=n;i++)
+        {
+            if(is_prime(i))
+            {
+                primeNumbers.add(i);
+            }
+        }
+        
+        // calculating sum of prime numbers
+        long sum = 0;
+        for(int i=0;i<primeNumbers.size();i++)
+        {
+            sum += primeNumbers.get(i);
+        }
+        return sum;
+    }
+    
+    static boolean is_prime(int n)
+    {
+        for(int i=2;i*i<=n;i++)
+        {
+            if(n%i == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /* 
+     * Using sieve of eratosthenes algorithm
+     * T(c) -> O(n log(logn)), space -> O(n)
+     */
+    public static long primeSumEfficient(int n)
+    {
+        boolean[] primes = new boolean[n+1];
+        Arrays.fill(primes, true);
+        // 0 and 1 are not prime, marking that as false
+        primes[0] = false;
+        primes[1] = false;
+        
+        // running loop till sqrt(n)
+        for(int i=2;i*i<=n;i++)
+        {
+            // if current number is prime
+            if(primes[i])
+            {
+                // marking all multiples of i as false(non prime)
+                for(int j=i*i;j<=n;j+=i)
+                {
+                    primes[j]=false;
+                }
+            }
+        }
+        
+        long sum = 0;
+        for(int i=2;i<=n;i++)
+        {
+            // if prime, add in sum
+            if(primes[i])
+            {
+                sum += i;
+            }
+        }
+        return sum;
+    }
+
+
     public static void main(String args[])
     {
         System.out.println("IsPrime 5? = "+ isPrime(5));
         System.out.println("IsPrime 9? = "+ isPrime(9));
+        int n = 10;
+        System.out.println("Sum of primes till "+ n + " = " + prime_Sum(n));
+        System.out.println("Sum of primes (efficient) till "+ n + " = " + primeSumEfficient(n));
     }
 }
