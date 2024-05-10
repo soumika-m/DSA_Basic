b7efdd3203970665a7c45dc7a0cb7efbd54af5e0
soumika-m <soumika1999@gmail.com>
2024-05-10 12:12:25 +0530

    Added minimum sum (efficient) and sort colors problem

--- Week3/ArrayProblem/MinimumSum.java
+++ Week3/ArrayProblem/MinimumSum.java
@@ -10,7 +10,7 @@ public class MinimumSum {
  */
 
     /* it will not work for very big size of n
-     * T(c) -> O(n), S(c) -> O(1) 
+     * T(c) -> O(nlogn), S(c) -> O(1) 
      */
     static String findMinSum(int[] arr, int n) {
         // sort the array
@@ -35,10 +35,84 @@ public class MinimumSum {
 
         return Long.toString(firstNum + secondNum);
     }
+
+    /* 
+    * using string
+    * T(c) -> O(nlogn), S(c) -> O(n)
+    */
+    static String findMinSumEfficient(int[] arr, int n) {
+        // sort the array
+        Arrays.sort(arr);
+
+        StringBuilder firstNum = new StringBuilder();
+        StringBuilder secondNum = new StringBuilder();
+
+        // create first num by using even index numbers of array, and second number using odd index
+        for(int i=0;i<n;i++)
+        {
+            if(i%2 == 0)
+            {
+                firstNum.append(arr[i]);
+            }
+            else{
+                secondNum.append(arr[i]);
+            }
+        }
+
+        int j = firstNum.length()-1;
+        int k = secondNum.length()-1;
+
+        StringBuilder ans = new StringBuilder();
+        int carry = 0;
+
+        // if any of the number digits present while doing addition, or we have carry
+        while(j >= 0 || k >= 0 || carry > 0)
+        {
+            int sum = carry;
+            // if only firstnumber is left
+            if(j >= 0)
+            {
+                sum += (firstNum.charAt(j--) - '0');
+            }
+            // if only secondnumber is left
+            if(k >= 0)
+            {
+                sum += (secondNum.charAt(k--) - '0');
+            }
+            ans.append(sum % 10);
+            carry = sum / 10;
+        }
+
+        // reverse final string as we are appending number from left to right
+        ans.reverse();
+
+        // to remove leading zeros which will appear ahead of our sum, if all zeros keep last one
+        while(ans.length() > 1 && ans.charAt(0) == '0')
+        {
+            ans.deleteCharAt(0);
+        }
+
+        return ans.toString();
+    }
    
     public static void main(String[] args) {
-        int arr[] = {6, 8, 4, 5, 2, 3};
-        System.out.println(findMinSum(arr, arr.length));
-        
+        int arr1[] = {6, 8, 4, 5, 2, 3};
+        System.out.println(findMinSum(arr1, arr1.length));
+        int arr2[] = {0, 0, 0, 0, 0};
+        System.out.println(findMinSumEfficient(arr2, arr2.length));
     }
 }
+
+/* 
+carry = 0
+
+ [2,3,4,5,6,8]
+  2 4 6
++ 3 5 8
+ ----------
+  6 0 4
+
+ 12/10 = 1
+ 12%10 = 2 
+*/
+
---
+++ Week3/ArrayProblem/SortColors.java
@@ -0,0 +1,107 @@
+public class SortColors {
+/*
+ * Given an array nums with n objects colored red, white, or blue, sort them in-place so that 
+ * objects of the same color are adjacent, with the colors in the order red, white, and blue.
+ * We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+ * You must solve this problem without using the library's sort function.
+ * 
+ * https://leetcode.com/problems/sort-colors/description/
+ * 
+ * T(c) -> O(n), S(c) -> O(1)
+ */
+    static void sortColors(int[] nums) {
+        int cnt0 = 0, cnt1 = 0, cnt2 = 0;
+        
+        // counting how many 0, 1, 2 are there
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i] == 0)
+            {
+                cnt0++;
+            }
+            else if(nums[i] == 1)
+            {
+                cnt1++;
+            }
+            else{
+                cnt2++;
+            }
+        }
+        
+        // adding 0, 1 , 2 back to array in corresponding positions based on count
+        for(int j=0;j<nums.length;j++)
+        {
+            if(cnt0 > 0)
+            {
+                nums[j] = 0;
+                cnt0--;
+            }
+            else if(cnt1 > 0)
+            {
+                nums[j] = 1;
+                cnt1--;
+            }
+            else{
+                nums[j] = 2;
+                cnt2--;
+            }
+        }
+    }
+
+    /* 
+    * using dutch national flag algorithm
+    * T(c) -> O(n) , S(c) -> O(1)
+    */
+    static void sortColorsEfficient(int[] nums) {
+
+        int low = 0;
+        int mid = 0;
+        int high = nums.length-1;
+        
+        while(mid <= high)
+        {
+            // if 0 found, swap low with mid and increase low and mid pointer
+            if(nums[mid] == 0)
+            {
+                int temp = nums[low];
+                nums[low] = nums[mid];
+                nums[mid] = temp;
+
+                mid++;
+                low++;
+            }
+            // if 1 found, nothing to do, just increase mid pointer
+            else if(nums[mid] == 1)
+            {
+                mid++;   
+            }
+            // if 2 found, swap high with mid, decrease high
+            else{
+                int temp = nums[high];
+                nums[high] = nums[mid];
+                nums[mid] = temp;
+                
+                high--;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int nums1[] = {2,0,2,1,1,0};
+        sortColors(nums1);
+        for(int i=0;i<nums1.length;i++)
+        {
+            System.out.print(nums1[i] + " ");
+        }
+        System.out.println();
+
+        int nums2[] = {2,0,1};
+        sortColorsEfficient(nums2);
+        for(int i=0;i<nums2.length;i++)
+        {
+            System.out.print(nums2[i] + " ");
+        }
+        System.out.println();
+    }
+    
+}
