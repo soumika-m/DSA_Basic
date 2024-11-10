dde88e6ffb3a0d543053d50dc47924dd6011a595
Soumika Mukherjee <48355954+soumika-m@users.noreply.github.com>
2024-11-10 21:18:14 +0530

    Update LargestPrimeFactor.py

--- Week1/MathProblem/Python/LargestPrimeFactor.py
+++ Week1/MathProblem/Python/LargestPrimeFactor.py
@@ -4,7 +4,28 @@ Given a number N, the task is to find the largest prime factor of that number.
 https://www.geeksforgeeks.org/problems/largest-prime-factor2601/1
 """
 
+
 def largestPrimeFactor(N):
+    """ T(c) -> O(sqrt(n)) , S(c) -> O(1) """
+    largest_prime = -1
+    i = 2
+    # a prime factor of number can not be more than sqrt(n)
+    while i*i <= N:
+        # if number is divisible by i, keep track of that i (2,3,...), divide that number with the divisor to get a odd number.
+        while N % i == 0:
+            largest_prime = i
+            N = N//i
+        
+        i += 1
+    
+    # if number is greater than 1, that is prime factor
+    if N > 1:
+        largest_prime = N
+        
+    return largest_prime
+
+
+def largestPrimeFactor2(N):
     """ T(c) -> O(sqrt(n)) , S(c) -> O(1) """
     i=2
     # for non prime number we will have atleast one factor <= sqrt(n)
@@ -19,3 +40,5 @@ def largestPrimeFactor(N):
 
 N  = 24
 print(largestPrimeFactor(N))
+N  = 5
+print(largestPrimeFactor2(N))
